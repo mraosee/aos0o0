185a6fd28deeb60a8404dae76701957688e6c2b6
mraosee <aoss.aasder@mail.ru>
2016-12-23 12:49:53 +0300

    Add files via upload

---
+++ plugins/TH3BOSS6.lua
@@ -0,0 +1,48 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                       ▀▄ ▄▀ 
+▀▄ ▄▀       BY MOHAMMED HISHAM              ▀▄ ▄▀ 
+▀▄ ▄▀ BY MOHAMMEDHISHAM (@TH3BOSS)          ▀▄ ▄▀          
+▀▄ ▄▀ JUST WRITED BY MOHAMMED HISHAM        ▀▄ ▄▀   
+▀▄ ▄▀          dev   : dev                  ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+—]] 
+do
+
+function run(msg, matches)
+return [[
+
+▫️الـبـوت الـذي يـعـمـل عـلـى مـجـمـوعـات الـسـوبـر 
+
+▫️يـعـمـل الـبـوت عـلـى مـجـمـوعـات سـوبـر تـصـل الـى 5 K عـضـو 
+
+   ≪ تـم صـنـع الـبـوت بـواسـطـه الـمـطـور ≫
+   -- Dev💀: @AOSEE_TH
+-- 
+-- DevChannel 💀: @AOSEE_THT
+
+                     
+                      
+
+                      
+▫️كـل مـا هـو جـديـد عـلـى قـنـاه الـسـورس 
+                      
+                      
+                      
+▫️للاسـتفـسـار راسـل الـمـطـور :                                     
+                    -- Dev💀: @AOSEE_TH
+-- 
+-- DevChannel 💀: @AOSEE_THT
+️
+]]
+end
+
+return {
+description = "Shows bot q", 
+usage = "spam Shows bot q",
+patterns = {
+"^(المطور)$",
+},
+run = run 
+}
+end
